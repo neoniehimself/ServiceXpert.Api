a6d1d8ef0b77b402d6087e11cfce70a501c83b2e
NeonieQuell <neoniequell.ponce@yahoo.com>
2025-04-27 15:27:50 -0400

    Remove isSkipCreateDate

--- ServiceXpert.Application/DataObjects/DataObjectBase.cs
+++ ServiceXpert.Application/DataObjects/DataObjectBase.cs
@@ -4,19 +4,4 @@ public abstract class DataObjectBase
     public DateTime CreateDate { get; set; }
 
     public DateTime? ModifyDate { get; set; }
-
-    protected DataObjectBase()
-    {
-        this.CreateDate = DateTime.UtcNow;
-    }
-
-    protected DataObjectBase(bool isSkipCreateDate = false)
-    {
-        if (!isSkipCreateDate)
-        {
-            this.CreateDate = DateTime.UtcNow;
-        }
-
-        this.ModifyDate = DateTime.UtcNow;
-    }
 }
