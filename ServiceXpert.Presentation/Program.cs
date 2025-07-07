b3932dfef3fb469c42b8e275ab3f3dac319ea1d8
NeonieQuell <neoniequell.ponce@yahoo.com>
2025-07-08 01:59:42 +0800

    Enable AppendTrailingSlash in RouteOptions

--- ServiceXpert.Presentation/Program.cs
+++ ServiceXpert.Presentation/Program.cs
@@ -9,6 +9,8 @@ builder.Services.AddApplicationLayerServices().AddInfrastructureLayerServices();
 
 builder.Services.AddControllers(options => options.ReturnHttpNotAcceptable = true);
 
+builder.Services.Configure<RouteOptions>(options => options.AppendTrailingSlash = true);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
