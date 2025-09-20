7036d9a5320c512771a65460695598f2a7adf7f9
neoniehimself <neoniequell.ponce@yahoo.com>
2025-09-21 04:46:44 +0800

    Fix Search

--- ServiceXpert.Infrastructure/Repositories/AspNetUserProfileRepository.cs
+++ ServiceXpert.Infrastructure/Repositories/AspNetUserProfileRepository.cs
@@ -15,11 +15,13 @@ public class AspNetUserProfileRepository : RepositoryBase<Guid, AspNetUserProfil
 
     public async Task<IEnumerable<AspNetUserProfile>> SearchUserByName(string searchQuery)
     {
+        searchQuery = searchQuery.Trim().Replace(' ', '%');
         searchQuery = $"%{searchQuery}%";
 
         return await this.dbContext.AspNetUserProfiles
-            .FromSqlInterpolated(@$"SELECT TOP 5 * FROM [AspNetUserProfiles]
-                WHERE [FirstName] LIKE {searchQuery} OR [LastName] LIKE {searchQuery}")
+            .FromSqlInterpolated(@$"
+SELECT TOP 5 * FROM [AspNetUserProfiles]
+WHERE CONCAT([FirstName], ' ', [LastName]) LIKE {searchQuery}")
             .TagWith($"{nameof(AspNetUserProfileRepository)}.{nameof(SearchUserByName)}")
             .ToListAsync();
     }
