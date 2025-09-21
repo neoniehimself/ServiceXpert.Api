8751c9e9337864b7c8f5eb60d7430cdd67b7e3ec
neoniehimself <neoniequell.ponce@yahoo.com>
2025-09-22 03:17:17 +0800

    Infrastructure

--- ServiceXpert.Infrastructure/Migrations/20250921191139_AddIssueReporter.Designer.cs
+++ ServiceXpert.Infrastructure/Migrations/20250921191139_AddIssueReporter.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ServiceXpert.Infrastructure.DbContexts;
 
@@ -11,9 +12,11 @@ using ServiceXpert.Infrastructure.DbContexts;
 namespace ServiceXpert.Infrastructure.Migrations
 {
     [DbContext(typeof(SxpDbContext))]
-    partial class SxpDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250921191139_AddIssueReporter")]
+    partial class AddIssueReporter
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
